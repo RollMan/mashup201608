d3a1b623a85740e9d350f606c27830c046f86b1c
RollMan <qwpmb554@gmail.com>
2016-08-20 17:40:23 +0900

    Move calcMesh.js to lib/url.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var mongoose = require('mongoose');
+var app = require('express')();
 
 var db = mongoose.connect('mongodb://localhost/player');
 
@@ -9,6 +10,36 @@ var PlayerSchema = new  mongoose.Schema({
   lord : String,
   ownmesh : [Number],
   people : Number
+});
+
+
+app.get('/', function(req, res){
+  console.log('request reached');
+  res.send('Hello world');
+});
+app.get('/employ', function(req, res){
+});
+
+/*
+ * {
+ *   id: ID,
+ *   latitude : LATITUDE,
+ *   longitude : LONGITUDE
+ * }
+ * 
+*/
+app.get('/position', function(req, res){
+  var q = req.query;
+  if(!(q.id && q.latitude && q.longitude)){
+    res.status(400).send();
+  }
+  console.log(req.query);
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
 /*
